3ab847c3ea05f19db2654f0a18e90e1ba5ee2edd
erman dell <ermangarciacano@gmail.com>
2023-10-23 15:54:31 -0300

    se agrego temporizador

--- js/memoria.js
+++ js/memoria.js
@@ -6,16 +6,35 @@ let resultado1 = null;
 let resultado2 = null;
 let movimientos = 0;
 let aciertos = 0;
-
+let temporizador = false;
+let timer = 30;
 
 let mostrarMovimientos = document.getElementById("movimientos");
 let mostarAciertos = document.getElementById("aciertos");
+let mostarTiempo = document.getElementById("tiempo");
 
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 numeros = numeros.sort(() => {return Math.random()-0.5});
 console.log(numeros);
 
+function contarTiempo(){
+    setInterval(() => {
+        timer--;
+        mostarTiempo.innerHTML = `Tiempo: ${timer} segundos`;
+
+        if(timer==0){
+            alert("se acabo el tiempo");
+        }
+
+    }, 1000)
+}
+
 function destapar(id){
+    if(temporizador == false){
+        contarTiempo();
+        temporizador = true;
+    }
+
     tarjetaDestapada++;
 
     if(tarjetaDestapada == 1){
@@ -37,7 +56,7 @@ function destapar(id){
             mostarAciertos.innerHTML = `Aciertos: ${aciertos}`;
             
             if(aciertos==8){
-                
+
             }
 
         }else(
